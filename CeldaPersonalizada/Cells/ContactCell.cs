2af53aaab95bc4b231bcdf18f1aa3e14ee5e63c6
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-17 11:37:04 -0500

    Custom cell

---
+++ CeldaPersonalizada/Cells/ContactCell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CeldaPersonalizada.Cells
+{
+    public class ContactCell : ViewCell
+    {
+
+        public static readonly BindableProperty ContactNameProperty =
+            BindableProperty.Create(nameof(ContactName), typeof(string), typeof(ContactCell), default(string));
+
+        public string ContactName
+        {
+            get { return (string)GetValue(ContactNameProperty); }
+            set { SetValue(ContactNameProperty, value); }
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            string name = BindingContext as string;
+            if(name!= null)
+            {
+                ContactName = name;
+            }
+        }
+    }
+}
