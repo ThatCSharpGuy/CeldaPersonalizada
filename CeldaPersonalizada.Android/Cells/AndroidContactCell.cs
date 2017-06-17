0135dafee8f4152abc121407de6e696b35d57543
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-17 13:26:52 -0500

    Renderers

---
+++ CeldaPersonalizada.Android/Cells/AndroidContactCell.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using CeldaPersonalizada.Cells;
+
+namespace CeldaPersonalizada.Droid.Cells
+{
+    public class AndroidContactCell : LinearLayout, INativeElementView
+    {
+        ContactCell _nativeContactCell;
+        public Element Element => _nativeContactCell;
+
+        TextView _firstLetterTextView;
+        TextView _fullNameTextView;
+
+        public AndroidContactCell(Context context, ContactCell cell) : base(context)
+        {
+            _nativeContactCell = cell;
+
+            var view = (context as Activity).LayoutInflater.Inflate(Resource.Layout.ContactCell, null);
+            _firstLetterTextView = view.FindViewById<TextView>(Resource.Id.FirstLetter);
+            _fullNameTextView = view.FindViewById<TextView>(Resource.Id.FullName);
+
+            AddView(view);
+        }
+
+        public void UpdateCell(string newName)
+        {
+            _firstLetterTextView.Text = newName.Substring(0, 1);
+            _fullNameTextView.Text = newName;
+
+        }
+    }
+}
---
+++ CeldaPersonalizada.Android/Cells/ContactCellRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Android.Content;
+using Android.Views;
+using Xamarin.Forms;
+using CeldaPersonalizada.Cells;
+using CeldaPersonalizada.Droid.Cells;
+using Xamarin.Forms.Platform.Android;
+using System.ComponentModel;
+using AndroidRelativeLayout = Android.Widget.RelativeLayout;
+using TextView = Android.Widget.TextView;
+
+[assembly: ExportRenderer(typeof(ContactCell), typeof(ContactCellRenderer))]
+namespace CeldaPersonalizada.Droid.Cells
+{
+    class ContactCellRenderer : ViewCellRenderer
+    {
+        AndroidContactCell _cell;
+
+        protected override Android.Views.View GetCellCore(Cell item, Android.Views.View convertView, ViewGroup parent, Context context)
+        {
+            var formsCell = Cell as ContactCell;
+            _cell = convertView as AndroidContactCell;
+            if (_cell == null)
+            {
+                _cell = new AndroidContactCell(context, formsCell);
+            }
+
+            _cell.UpdateCell(formsCell.ContactName);
+
+            return _cell;
+        }
+
+        protected override void OnCellPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var formsCell = (ContactCell)sender;
+            if (e.PropertyName.Equals("ContactName"))
+            {
+                _cell.UpdateCell(formsCell.ContactName);
+            }
+        }
+    }
+}
